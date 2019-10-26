f9ce7b324932327add8f3a5e8d03521b918870de
julyseng <julyseng@gmail.com>
2019-10-26 19:24:17 +1300

    add quotes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -10,6 +10,14 @@ function App() {
       <h2 className='subtitle'> Your local lawn mower </h2>
 
       <p> Lawn care, gardening, trimming</p>
+      <p className='Quotes'>Free quotes</p>
+        <ul>
+          <li>Gardening</li>
+          <li>Lawn Mowing</li>
+          <li>Section Tidy</li>
+          <li>Grass planting</li>
+          <li>House cleaning</li>
+        </ul>
       </header>
     </div>
   );
