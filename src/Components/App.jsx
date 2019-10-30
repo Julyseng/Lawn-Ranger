dcc39b3e2e719cea06afd23af077b8407cfeca84
julyseng <julyseng@gmail.com>
2019-10-30 13:59:43 +1300

    linking components

---
+++ src/Components/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+// import logo from './logo.svg';
+import '../App.css';
+import Contact from './Contact'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1 className='welcome'>Welcome to Lawn Ranger</h1>
+      <h2 className='subtitle'> Your local lawn mower </h2>
+
+      <p> Lawn care, gardening, trimming</p>
+      <p className='Quotes'>Free quotes</p>
+        <ul>
+          <li>Gardening</li>
+          <li>Lawn Mowing</li>
+          <li>Section Tidy</li>
+          <li>Grass planting</li>
+          <li>House cleaning</li>
+          <li>Flower Planting</li>
+        </ul>
+      </header>
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,1 +1,16 @@
 import React from 'react';
+
+
+class Contact extends React.Component {
+    render () {
+        return (
+            <div className='contacts'>
+                <p> Contact now</p>
+
+            </div>
+        )
+    }
+
+}
+
+export default Contact
