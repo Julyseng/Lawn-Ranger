6501085acdb112dd57fca99eec475d598d3df990
julyseng <julyseng@gmail.com>
2019-10-30 16:24:16 +1300

    add gallery component

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -4,8 +4,8 @@ import Contact from './Contact'
 import About from './About'
 import Home from './Home'
 import Services from './Services'
+import Gallery from './Gallery'
 import { HashRouter as Router, Route, Switch, Link } from 'react-router-dom'
-
 
 class App extends Component {
   render() {
@@ -18,6 +18,7 @@ class App extends Component {
               <li><Link to={'/'} className="nav-link"> Home </Link></li>
               <li><Link to={'/about'} className="nav-link">About</Link></li>
               <li><Link to={'/services'} className="nav-link">Services</Link></li>
+              <li><Link to={'/gallery'} className="nav-link">Gallery</Link></li>
               <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
             </ul>
           </nav>
@@ -26,6 +27,7 @@ class App extends Component {
             <Route exact path='/' component={Home} />
             <Route path='/contact' component={Contact} />
             <Route path='/about' component={About} />
+            <Route path='/gallery' component={Gallery} />
             <Route path='/services' component={Services} />
           </Switch>
         </div>
---
+++ src/Components/Gallery.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Gallery extends Component {
+    render() {
+        return(
+            <React.Fragment> 
+            <h3>Photo Gallery</h3>
+            <img src='https://www.4seasonslawncare.co.uk/wp-content/uploads/2019/04/Aeration-before-after.jpg' alt='lawn'/>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Gallery
