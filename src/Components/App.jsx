8cbc57331b80f0d4af92b6394f9c7e54c023f647
julyseng <julyseng@gmail.com>
2019-10-31 17:03:10 +1300

    adds logo

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -5,6 +5,7 @@ import About from './About'
 import Home from './Home'
 import Services from './Services'
 import Gallery from './Gallery'
+import Pricing from './Pricing'
 import { HashRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
 class App extends Component {
@@ -12,12 +13,12 @@ class App extends Component {
     return (
       <Router>
         <div>
-          <h2>Lawn Ranger</h2>
-
+          <img className='logo' src='./logo-img.png' alt='logo' />
             <ul>
               <li><Link to={'/'} className="nav-link"> Home </Link></li>
               <li><Link to={'/about'} className="nav-link">About</Link></li>
               <li><Link to={'/services'} className="nav-link">Services</Link></li>
+              <li><Link to={'/pricing'} className="nav-link">Pricing</Link></li>
               <li><Link to={'/gallery'} className="nav-link">Gallery</Link></li>
               <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
             </ul>
@@ -26,6 +27,7 @@ class App extends Component {
             <Route exact path='/' component={Home} />
             <Route path='/contact' component={Contact} />
             <Route path='/about' component={About} />
+            <Route path='/pricing' component={Pricing} />
             <Route path='/gallery' component={Gallery} />
             <Route path='/services' component={Services} />
           </Switch>
